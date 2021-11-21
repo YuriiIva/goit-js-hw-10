82f32f79e27ea8e1eb8220bd73b644f3b143b175
YurkaBest <Ivaschenko_u@ukr.net>
2021-11-21 18:38:43 +0200

    finish with 1 bug

--- src/js/index.js
+++ src/js/index.js
@@ -16,9 +16,32 @@ const refs = {
 // fetchCountries().then(users => createUsersMarkupLi(users));
 
 const createUsersMarkupLi = obj => {
-  const markup = obj.map(({ name: { official } }) => `<li><p>${official}</p></li>`).join('');
+  const markup = obj
+    .map(({ name: { official } }) => `<li class="js-item"><p>${official}</p></li>`)
+    .join('');
   console.log(markup);
   refs.list.innerHTML = markup;
+};
+
+const createMarkupList = dataList => {
+  const markupList = dataList
+    .map(
+      ({ name: { official }, capital, population, flags: { svg }, languages }) =>
+        `<div class ="js-list">
+        <div>
+        <img src="${svg}" alt="flag" width="20" class ="js-img">
+        </div>
+        <h1>${official}</h1>
+        </div>
+<ul>
+  <li class="js-item">Capital:<span class ="js-icon">${capital}</span></li>
+  <li class="js-item">Population:<span class ="js-icon">${population}</span></li>
+  <li class="js-item">Languages:<span class ="js-icon">${languages}</span></li>
+</ul>
+`,
+    )
+    .join('');
+  refs.info.innerHTML = markupList;
 };
 
 const handleCountryInput = e => {
@@ -28,10 +51,15 @@ const handleCountryInput = e => {
   fetchCountries(inputName)
     .then(data => {
       if (data.length > 10) {
-        alert(`Oops,more 10`);
+        notiflix.Notify.failure(`Too many matches found. Please enter a more specific name.`);
         return;
       }
-      createUsersMarkupLi(data);
+      if (data.length < 10 && data.length > 1) {
+        createUsersMarkupLi(data);
+      }
+      if (data.length === 1) {
+        createMarkupList(data);
+      }
     })
     .catch(err => {
       notiflix.Notify.failure(`Oops, there is no country with that name`);
